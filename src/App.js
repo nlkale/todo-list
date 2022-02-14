c5e1051c2902ae681fc357f8576f5cf1609fe33e
Mykola Kosynets <kolyan.kosenez@gmail.com>
2022-02-14 14:37:58 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <input type="text" placeholder="Напишіть щось" />
+        <button className="enter">ДОДАТИ</button>
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
